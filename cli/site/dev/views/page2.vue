17c0d5824fd1f5adbf693c6ab7117fbac266fe82
jojo-f <jojo@dcpt.online>
2020-10-14 15:24:21 +0800

    修复滚动通知样式

--- cli/site/dev/views/page2.vue
+++ cli/site/dev/views/page2.vue
@@ -8,7 +8,7 @@
       </Header>
     </template>
 
-    <!-- <notice-bar left-icon="volume-o" text="在代码阅读过程中人们说脏话的频率是衡量代码质量的唯一标准。在代码阅读过程中人们说脏话的频率是衡量代码质量的唯一标准。" /> -->
+    <notice-bar left-icon="volume-o" text="在代码阅读过程中人们说脏话的频率是衡量代码质量的唯一标准。在代码阅读过程中人们说脏话的频率是衡量代码质量的唯一标准。" />
 
     <pullup-loading :reqMethod="request">
       <div v-for="(item, index) in 100" :key="index">
